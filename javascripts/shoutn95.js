922f9791730c16f34168a9812ab7fd619f12200d
shoutn95 <shoutn95@users.noreply.github.com>
2020-07-30 21:27:34 +0200

    shoutn95.js

--- javascripts/shoutn95.js
+++ javascripts/shoutn95.js
@@ -116,7 +116,7 @@ shoutn95._getFront0withLatestContent=function()
          if(k==content) break;
          content_.push(shoutn95.url+"contents/"+"CONTENTS"+k+".md");
       }
-      //console.log(content);
+      //console.log(content_);
       
       if(shoutn95.GetURLParameter('page')==0)
       {
@@ -126,15 +126,18 @@ shoutn95._getFront0withLatestContent=function()
          var md_ = window.markdownit("default",{html:true});      
                for( var n=0;n<content_.length;n++){
                //console.log(content_);
-                  if(n=>0){
+                  if(n>=0){
+                     //console.log(n);
                   jQuery.get(content_[n],function(data){
                      html_.push($(md_.render(data)).html());
+                     console.log(html_);
                      if(n==html_.length){
+                        console.log("ok");
                         content-=1;
-                        html_=html_.sort().reverse();
-                        console.log(html_);
+                        html_=html_.sort();
+                        //console.log(html_);
                         for(j=0;j<html_.length;j++){
-                            //console.log(content);
+                            console.log(content);
                            $("#_latestContent ").append("<a href='"+shoutn95.url+"?page="+content+"'>"+"##"+html_[j]+"</a><br />");
                            content-=1;
                         }   
