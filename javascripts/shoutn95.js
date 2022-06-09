67d9e5ad9c8ce49106181c3d9205f6ae240ee192
shoutn95 <shoutn95@gmail.com>
2022-06-09 17:48:57 +0200

    video

--- javascripts/shoutn95.js
+++ javascripts/shoutn95.js
@@ -51,23 +51,6 @@ shoutn95.loadFrontJSONform=function()
         $("<div id='front'></div>").insertBefore("#top");
         $("#front").html(shoutn95.loadFront(video));
       }
-      var $window = $(window);
-      var $videoWrap = $('.video-wrap');
-      var $video = $('.video');
-      var videoHeight = $video.outerHeight();
-
-      $window.on('scroll',  function() {
-          var windowScrollTop = $window.scrollTop();
-          var videoBottom = videoHeight + $videoWrap.offset().top;
-
-          if (windowScrollTop > videoBottom) {
-            $videoWrap.height(videoHeight);
-            $video.addClass('stuck');
-          } else {
-              $videoWrap.height('auto');
-              $video.removeClass('stuck');
-            }
-          });
    }
 }
 
