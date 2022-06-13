e019f402ad3b28a348e9bd5ab0660a537ddc856a
shoutn95 <shoutn95@gmail.com>
2022-06-13 13:04:43 +0200

    Update _instagram_.js

--- javascripts/_instagram_.js
+++ javascripts/_instagram_.js
@@ -27,11 +27,10 @@ _instagram_.get_media_url = function()
 
             //if(shoutn95.GetURLParameter('page')==0){
               // $("#latest_instagram").html("<table><tr><td><b>Nouvelles from instagram <u>@shoutn95</u></b></td></tr><tr><td></td></tr></table>");
-            }//
+        }//
             //$("#latest_instagram").html("<img src='"+instagram+"' />");
             //return instagram;
         }
-  }
 }
 _instagram_.PARSE_Media_IDfromJSON=function()
 {
